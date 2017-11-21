edbb51c54aef8dbb60d7fb2fc90429234bc80ff5
jessmitch42 <jessicamitch42@gmail.com>
2017-11-21 17:02:22 -0500

    commiting plan for order of events

--- lib/is_it_poly/cli.rb
+++ lib/is_it_poly/cli.rb
@@ -3,6 +3,14 @@
 class IsItPoly::CLI
 
   def call
-    puts "well is it??"
+    # message to user
+    # let them know to type "exit" at any point to exit the program
+    # first pick a store you want to look at
+    # scrape info (first level) from the store (don't permanently save it in case it changes)
+    # first level info: product name, brand, whatever is available on general list page (keep it brief for list view)
+    # second level info: product name, description, price, any specific data for individual item
+      # needs the option to ask "is it poly?" to give yes or no answer
+    # "back" command to go level up?
+
   end
 end
